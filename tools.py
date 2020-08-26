a98b60b79ed3dcb0d55d5d78749bcf366f572984
Hydra NET <67011201+hydra20@users.noreply.github.com>
2020-08-26 04:45:03 +0100

    Update tools.py

--- tools.py
+++ tools.py
@@ -94,7 +94,7 @@ elif choose == "5":
     os.system ('python2 tools.py')
 
 elif choose == "6":
-	os.system('git clone')
+	os.system('git bush origin master')
 	print ("\033[1;32mSuccessfully updated")
 	time.sleep (2)
 	os.system ('python2 tools.py')
